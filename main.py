b35c7238d7a70cc97bbe35d31eb2a0236c8b88cf
Letscrypto25 <letscrypto25@gmail.com>
2025-04-24 23:55:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import telegram
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    update.message.reply_text("Hello! Your bot is working.")
+
+def main():
+    TOKEN = os.getenv("BOT_TOKEN")
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
